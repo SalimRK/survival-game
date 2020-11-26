e4819d39a7845fd8309bcc4c0b106524a4ea1eaa
Salim RK <70140026+SalimRK@users.noreply.github.com>
2020-11-26 13:58:50 +0200

    some random stuff
    i realy dint know what that is lol

---
+++ survival game/Assets/coding/pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public Animator animator;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("PickUp"))
+        {
+            animator.SetBool("pickup", true);
+        }
+        else
+        {
+            animator.SetBool("pickup", false);
+        }
+    }
+}
